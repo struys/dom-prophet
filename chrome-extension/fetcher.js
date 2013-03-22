69b3f1395a5278c94fb602c244c3d39339f9b0c6
Bhargav Nookala <nooknb@gmail.com>
2013-03-22 01:47:42 -0700

    adding timestamp data

--- chrome-extension/fetcher.js
+++ chrome-extension/fetcher.js
@@ -63,6 +63,7 @@
 
         var logLine = {
             eventType: e.type,
+	    timeStamp: e.timeStamp,
             elements: [],
             url: document.location.href,
             cookie: document.cookie,
